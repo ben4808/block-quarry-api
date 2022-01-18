8586a3b5dc86f973e7b2990598cc7fbd6cc30a27
Ben Zoon <ben.zoon@navexglobal.com>
2022-01-18 01:18:01 -0800

    fix cors

--- src/Server.ts
+++ src/Server.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://blockquarry.net"],
+    origin: ["http://localhost:3000", "https://blockquarry.net"],
     credentials: true,
 }));
 
